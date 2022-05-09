66ec13ec21555effd2df91f146612cdfde021a42
berkagmpp <berkagmpp@gmail.com>
2022-05-09 17:27:43 +1200

    make requestConfig obj be flexible in useHTTP custom hook

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -11,9 +11,9 @@ const useHttp = (requestConfig, applyData) => {
             const response = await fetch(
                 requestConfig.url, 
                 {
-                    method: requestConfig.method,
-                    body: JSON.stringify(requestConfig.body),
-                    headers: requestConfig.headers
+                    method: requestConfig.method ? requestConfig.method: 'GET',
+                    body: requestConfig.body ? JSON.stringify(requestConfig.body): null,
+                    headers: requestConfig.headers ? requestConfig.headers: {}
                 }
             );
 
